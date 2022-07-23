bd34e082dcd17552d23702bdbefbf2569fab97d8
Christopher Okoro <okorochristopher20@gmail.com>
2022-07-23 10:28:50 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,13 +6,6 @@ export default function Header({ title }) {
       Please fill in your details below
       </p>
 
-      /**<p>
-        You can find the code for this project on GitHub at{" "}
-        <a href="https://github.com/netlify-templates/nextjs-toolbox">
-          https://github.com/netlify-templates/nextjs-toolbox
-        </a>
-        ! Happy coding!
-      </p>*/
     </>
   );
 }
