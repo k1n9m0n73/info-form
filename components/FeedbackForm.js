7f9f591e9c0b26f682fc0a95aefc09d6b2d28394
Christopher Okoro <okorochristopher20@gmail.com>
2022-07-23 21:56:35 +0100

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -8,7 +8,7 @@ export default function FeedbackForm() {
     <title>Data Form</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
   </head>
   <body>
     <div className={styles.form}>
