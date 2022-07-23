6bec2dfe81620b78899560107c74e8a7ac4b1c07
Christopher Okoro <okorochristopher20@gmail.com>
2022-07-23 18:03:57 +0000

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -7,8 +7,7 @@ export default function FeedbackForm() {
 	<head>
     	<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<title>Data Form</title>
-	  </head>
+    	<title>Data Form</title></head>
 	<body>
 	  
       <form
