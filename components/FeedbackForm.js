71310f8c24ea58e0b82dc08f02f4001319b65e98
Christopher Okoro <okorochristopher20@gmail.com>
2022-07-29 15:51:40 +0000

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -4,7 +4,12 @@ import Script from 'next/script'
 export default function FeedbackForm() {
   return ( 
 	 <html lang="en">
+	        <head>
+        	<meta charset="utf-8" />
+	    	<meta name="viewport" content="width=device-width, initial-scale=1" />
+        	</head>
 	  <body>
+	  <Script src="/components/js/lga.js"></Script>
  	 <form
                       className={styles.form}
                       data-netlify="true"
